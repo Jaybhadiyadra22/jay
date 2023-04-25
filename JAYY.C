fecc4f1deb97c779831e3b6d13bbec0e79863a09
Jaybhadiyadra22 <131762319+Jaybhadiyadra22@users.noreply.github.com>
2023-04-25 15:26:49 +0530

    Add files via upload

---
+++ JAYY.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{  clrscr();
+int a,b,c,d,e,f,g;
+printf("enter value A=");
+scanf("%d",&a);
+printf("enter value B=");
+scanf("%d",&b);
+
+printf("addition of A+B=%d/n",a+b);
+printf("addition of A-B=%d/n",a-b);
+printf("aadition of A*B=%d/n",a*b);
+printf("addition of A/B=%d/n",a/b);
+
+getch();
+}
